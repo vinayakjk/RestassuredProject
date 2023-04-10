2ed61592b6c0e6bce1e1d64dd58fe610d5054e94
Vinayak <vinayak.jalanakar@gmail,com>
2023-04-10 15:54:04 +0530

    This is my first checkin

---
+++ src/test/java/day1/HTTPRequest.java
@@ -0,0 +1,56 @@
+package day1;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+import org.testng.annotations.*;
+
+/*
+ 	Given()
+ 		prerequisites like headers,parameters,keys,etc
+ 	.When()
+ 		execute GET,POST,PUT,DELETE http requests.
+
+ 	.Then()
+ 		validate the response codes,response body,etc
+
+ */
+public class HTTPRequest {
+
+	//Test case to GET the data of user
+
+	@Test (enabled = true)
+	public void getUsersData()
+	{
+		given()
+		.when().get("https://reqres.in/api/users?page=2")
+		.then()
+		.statusCode(200).body("page",equalTo(2))
+		.log().all();
+	}
+
+	//Test case to create user
+	
+	@Test (priority = 1)
+	public void creatUsers()
+	{
+		HashMap data=new HashMap();
+		data.put("name", "Vinayak");
+		data.put("job", "QA-Lead");
+		
+		given()
+		.when()
+		.contentType("application/JSON")
+		.body(data)
+		
+		.then()
+		.statusCode(201)
+		.log().all();
+		
+	}
+
+
+
+}
